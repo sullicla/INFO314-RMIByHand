e2ae9833e6d0a7f275c9cfc4ef00a77e115c7aa9
sulli <sullicla@uw.edu>
2023-05-01 21:09:57 -0700

    first update RMIByHand

--- Java/Client.java
+++ Java/Client.java
@@ -4,20 +4,69 @@ public class Client {
      * This method name and parameters must remain as-is
      */
     public static int add(int lhs, int rhs) {
-        return -1;
+
+        RemoteMethod add = new RemoteMethod("add", new Object[]{lhs, rhs});
+        return (int) makeRequest(add);
+
+        // connect to server
+        // create an instance of the RemoteMethod
+        // RemoteMethod add = new RemoteMethod("add", new Objects[]{lhs, rhs});
+//        RemoteMethod add = new RemoteMethod("add", new Objects[]{lhs, rhs});
+        // ObjectOutputStream to serialize the add instance
+        // OutputStream os = socket.getOutputStream();
+//        return -1;
     }
+
     /**
      * This method name and parameters must remain as-is
      */
-    public static int divide(int num, int denom) {
-        return -1;
+    public static int divide(int num, int denom) throws ArithmeticException {
+
+        RemoteMethod div = new RemoteMethod("divide", new Object[]{num, denom});
+
+        Object response = makeRequest(div);
+        if (response instanceof ArithmeticException) throw (ArithmeticException) response;
+        return (int) response;
     }
     /**
      * This method name and parameters must remain as-is
      */
     public static String echo(String message) {
-        return "";
+
+        RemoteMethod echo = new RemoteMethod("echo", new Object[]{message});
+        return (String) makeRequest(echo);
+
     }
+
+    private static Object makeRequest(RemoteMethod requestMethod) {
+        try (Socket sock = new Socket("localhost", PORT)) {
+            // Create output stream and write the request method object to it
+            OutputStream sockOut = sock.getOutputStream();
+            ObjectOutputStream objOut = new ObjectOutputStream(sockOut);
+            objOut.writeObject(requestMethod);
+
+            // Create input stream and read the response object from it
+            InputStream sockIn = sock.getInputStream();
+            ObjectInputStream objIn = new ObjectInputStream(sockIn);
+            Object response = objIn.readObject();
+
+            // Check if the response object is an exception and throw it if so
+            if (response instanceof Exception) {
+                throw (Exception) response;
+            }
+
+            // Return the response object
+            return response;
+        } catch (ConnectException e) {
+            // If a ConnectException is caught, print an error message and return it
+            System.out.println("The server is down! I repeat, the server is down!!! (Can't reach localhost:" + PORT + ")");
+            return e;
+        } catch (Exception e) {
+            // If any other exception is caught, return it
+            return e;
+        }
+    }
+
 
     // Do not modify any code below this line
     // --------------------------------------
@@ -37,8 +86,7 @@ public class Client {
         try {
             divide(1, 0);
             System.out.print("X");
-        }
-        catch (ArithmeticException x) {
+        } catch (ArithmeticException x) {
             System.out.print(".");
         }
 
@@ -46,7 +94,7 @@ public class Client {
             System.out.print(".");
         else
             System.out.print("X");
-        
+
         System.out.println(" Finished");
     }
 }
--- Java/Server.java
+++ Java/Server.java
@@ -1,13 +1,76 @@
+import java.net.ServerSocket;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Server {
+    public static void main(String[] args) {
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+
+        try ( ServerSocket server = new ServerSocket(10314) ) {
+            while (true) {
+                Socket sock = server.accept();
+                threadPool.execute(() -> respond(sock));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void respond(Socket sock) {
+        try {
+            InputStream request = sock.getInputStream();
+            ObjectInputStream objInStream = new ObjectInputStream(request);
+            RemoteMethod requestMethod = (RemoteMethod) objInStream.readObject();
+
+            String methodName = requestMethod.getMethodName();
+            Object[] args = requestMethod.getArgs();
+
+            Method[] availableMethods = new Server().getClass().getMethods();
+            Method callMe = null;
+            for (Method method : availableMethods) {
+                if (method.getName().equals(methodName)) {
+                    callMe = method;
+                    break;
+                }
+            }
+
+            Object result = null;
+            if (callMe == null) {
+                result = new Exception("Couldn't find method " + methodName);
+            } else {
+                try {
+                    result = callMe.invoke(null, args);
+                } catch (InvocationTargetException e) {
+                    result = e.getCause();
+                }
+            }
+
+            OutputStream response = sock.getOutputStream();
+            ObjectOutputStream objOutStream = new ObjectOutputStream(response);
+            objOutStream.writeObject(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     // Do not modify any code below tihs line
     // --------------------------------------
-    public static String echo(String message) { 
+    public static String echo(String message) {
         return "You said " + message + "!";
     }
+
     public static int add(int lhs, int rhs) {
         return lhs + rhs;
     }
+
     public static int divide(int num, int denom) {
         if (denom == 0)
             throw new ArithmeticException();
